526b90a054e99c84754ab3348e2a6cfa67fd41a8
Poojani <poojaniranasinghe2000@gmail.com>
2023-03-26 14:00:18 +0530

    account controller created

--- InHomePlan/InHomePlanWeb/Controllers/HomeController.cs
+++ InHomePlan/InHomePlanWeb/Controllers/HomeController.cs
@@ -23,6 +23,15 @@ namespace InHomePlanWeb.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();  
+        }
+
+        public IActionResult Register()
+        {
+            return View();  
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
