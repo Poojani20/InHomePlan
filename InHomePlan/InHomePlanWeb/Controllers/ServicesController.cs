cae0d554d936f3fc36b337e40d85305b3b12d28d
Poojani <poojaniranasinghe2000@gmail.com>
2023-03-27 12:22:38 +0530

    services view and controller

---
+++ InHomePlan/InHomePlanWeb/Controllers/ServicesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InHomePlanWeb.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult Services()
+        {
+            return View();
+        }
+    }
+}
