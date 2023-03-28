220fce776fdfe32efc6667fabf8708e25d3c5439
Poojani <poojaniranasinghe2000@gmail.com>
2023-03-28 20:40:33 +0530

    adding controller for application

---
+++ InHomePlan/InHomePlanWeb/Controllers/ApplicationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InHomePlanWeb.Controllers
+{
+    public class ApplicationController : Controller
+    {
+        public IActionResult Application1()
+        {
+            return View();
+        }
+        public IActionResult Application2() 
+        { 
+            return View();
+        }
+    }
+}
