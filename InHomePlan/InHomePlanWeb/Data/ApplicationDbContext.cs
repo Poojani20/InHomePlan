6f3e8ad44d1fb4d12af18a4858e67ab4ee2e4fb7
Poojani <poojaniranasinghe2000@gmail.com>
2023-04-05 17:24:59 +0530

    configure dbContext and entity frameworkcore

--- InHomePlan/InHomePlanWeb/Data/ApplicationDbContext.cs
+++ InHomePlan/InHomePlanWeb/Data/ApplicationDbContext.cs
@@ -4,6 +4,9 @@ namespace InHomePlanWeb.Data
 {
     public class ApplicationDbContext : DbContext 
     {
-         
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options) 
+        {
+
+        }
     }
 }
