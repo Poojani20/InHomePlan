ff9aadd606c4e9188710629b0906f6c2614c9341
Poojani <poojaniranasinghe2000@gmail.com>
2023-04-07 05:16:56 +0530

    session model

---
+++ InHomePlan/InHomePlanWeb/Models/Session.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InHomePlanWeb.Models
+{
+    public class Session
+    {
+        public int SessionID { get; set; }
+        public string Token { get; set; } 
+        public DateTime Expiration { get; set; }
+
+        // Foreign key property
+        public int UserID { get; set; }
+
+        // Navigation property
+        [ForeignKey("UserID")]
+        public virtual User User { get; set; }
+
+    }
+}
