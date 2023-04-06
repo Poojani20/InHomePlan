ec0c1ef9ab9d4f7ffcc88a5be3ca7eb551f65846
Poojani <poojaniranasinghe2000@gmail.com>
2023-04-06 14:41:55 +0530

    surveyor model

---
+++ InHomePlan/InHomePlanWeb/Models/Surveyor.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InHomePlanWeb.Models
+{
+    public class Surveyor
+    {
+        [Key]
+        public int SurveyorID { get; set; }
+        public string FirstName { get; set;}
+        public string LastName { get; set;}  
+        public string Address { get; set;}
+        public string LicenseNo { get; set;}
+
+        // Foreign key property
+        public int UserID { get; set; }
+
+        // Navigation property
+        [ForeignKey("UserID")]
+        public virtual User User { get; set; }
+
+
+    }
+}
