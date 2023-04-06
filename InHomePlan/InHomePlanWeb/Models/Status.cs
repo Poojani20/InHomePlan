bee3e654f722f66139bf8c2f3e269befc502c160
Poojani <poojaniranasinghe2000@gmail.com>
2023-04-06 19:49:57 +0530

    status model

---
+++ InHomePlan/InHomePlanWeb/Models/Status.cs
@@ -0,0 +1,10 @@
+﻿namespace InHomePlanWeb.Models
+{
+    public class Status
+    {
+        public int StatusID { get; set; }
+        public string StatusName { get; set; }  
+        public string StatusDescription { get; set; }  
+        public DateTime Status_date { get; set; }
+    }
+}
