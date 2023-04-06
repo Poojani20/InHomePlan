25ae34a1844a5fe2c830e6b660b46a14ac75b01c
Poojani <poojaniranasinghe2000@gmail.com>
2023-04-07 05:25:07 +0530

    regional staff model

---
+++ InHomePlan/InHomePlanWeb/Models/RegionalStaff.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InHomePlanWeb.Models
+{
+    public class RegionalStaff
+    {
+        public int RegionalStaffID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; } 
+        public string Address { get; set; }
+        public string City { get; set; }    
+        public string State { get; set; }   
+        public string ZipCode { get; set; }
+        // Foreign key property
+        public int UserID { get; set; }
+
+        // Navigation property
+        [ForeignKey("UserID")]
+        public virtual User User { get; set; }
+
+    }
+}
