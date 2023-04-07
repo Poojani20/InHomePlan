b627823be3bdaa4ce4b49eddae833526126d55ae
Poojani <poojaniranasinghe2000@gmail.com>
2023-04-07 09:10:11 +0530

    inspection model

---
+++ InHomePlan/InHomePlanWeb/Models/Inspection.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InHomePlanWeb.Models
+{
+    public class Inspection
+    {
+        [Key]
+        public int InspectionID { get; set; }
+        public string InspectionStatus { get; set; }  
+        public string InspectionComment { get; set;}
+        public DateTime InspectionDate { get; set; }
+
+        [ForeignKey("Application")]
+        public int ApplicationID { get; set; }
+
+        public virtual Application Application { get; set; }
+
+    }
+}
