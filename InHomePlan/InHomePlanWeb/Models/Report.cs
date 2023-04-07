2410ee1addc82b2f66b302f5c7c3928cb92d48c8
Poojani <poojaniranasinghe2000@gmail.com>
2023-04-07 06:50:47 +0530

    report model

---
+++ InHomePlan/InHomePlanWeb/Models/Report.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InHomePlanWeb.Models
+{
+    public class Report
+    {
+        [Key]
+        public int ReportID { get; set; }
+        public string ReportName { get; set; }
+
+        [ForeignKey("Application")]
+        public int ApplicationID { get; set; }
+
+        public virtual Application Application { get; set; }
+
+
+    }
+}
