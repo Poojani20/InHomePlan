8f89b9dafab8c3970ac0fdbea69a6ac5a63a6ba4
Poojani <poojaniranasinghe2000@gmail.com>
2023-04-07 06:57:25 +0530

    architect model

---
+++ InHomePlan/InHomePlanWeb/Models/Architect.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace InHomePlanWeb.Models
+{
+    public class Architect
+    {
+        [Key]
+        public int ArchitectID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string LicenseNo { get; set; }
+
+        // Foreign key property
+        public int UserID { get; set; }
+
+        // Navigation property
+        [ForeignKey("UserID")]
+        public virtual User User { get; set; }
+
+    }
+}
