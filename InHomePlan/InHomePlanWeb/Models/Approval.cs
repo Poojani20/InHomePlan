e4a9517a0c6c31ba56840d89d0f03654de152945
Poojani <poojaniranasinghe2000@gmail.com>
2023-04-07 07:28:26 +0530

    approval model

---
+++ InHomePlan/InHomePlanWeb/Models/Approval.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InHomePlanWeb.Models
+{
+    public class Approval
+    {
+        [Key]
+        public int ApprovalID { get; set; }
+        public string ApprovedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+
+        [ForeignKey("Application")]
+        public int ApplicationID { get; set; }
+
+        public virtual Application Application { get; set; }
+    }
+}
