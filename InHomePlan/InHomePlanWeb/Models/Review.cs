e05ecc1762c2b382400c2adc3c55f8f03e977ec5
Poojani <poojaniranasinghe2000@gmail.com>
2023-04-07 06:32:08 +0530

    review model

---
+++ InHomePlan/InHomePlanWeb/Models/Review.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InHomePlanWeb.Models
+{
+    public class Review
+    {
+        public int ReviewID { get; set; }
+        public string Review_Comment { get; set;}
+        public string Review_Status { get; set; }
+        public DateTime Review_Date { get; set; }
+
+        [ForeignKey("Application")]
+        public int ApplicationID { get; set; }
+
+        public virtual Application Application { get; set; }
+    }
+}
