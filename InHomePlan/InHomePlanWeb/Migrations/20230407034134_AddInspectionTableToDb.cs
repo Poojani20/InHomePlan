6aba1765a52dd5981d350be7f32fee9c2a0a6964
Poojani <poojaniranasinghe2000@gmail.com>
2023-04-07 09:18:10 +0530

    inspection migration

---
+++ InHomePlan/InHomePlanWeb/Migrations/20230407034134_AddInspectionTableToDb.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InHomePlanWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInspectionTableToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Inspection",
+                columns: table => new
+                {
+                    InspectionID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    InspectionStatus = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    InspectionComment = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    InspectionDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ApplicationID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inspection", x => x.InspectionID);
+                    table.ForeignKey(
+                        name: "FK_Inspection_Application_ApplicationID",
+                        column: x => x.ApplicationID,
+                        principalTable: "Application",
+                        principalColumn: "ApplicationID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inspection_ApplicationID",
+                table: "Inspection",
+                column: "ApplicationID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Inspection");
+        }
+    }
+}
