f891f6bef5dc524e78ae3313bbc83569c373eae4
Poojani <poojaniranasinghe2000@gmail.com>
2023-04-07 09:34:33 +0530

    profile model

---
+++ InHomePlan/InHomePlanWeb/Models/Profile.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InHomePlanWeb.Models
+{
+    public class Profile
+    {
+        [Key]
+        public int ProfileID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+
+    }
+}
