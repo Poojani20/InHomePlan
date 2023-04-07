e4db8987b91f1a1d23ea174f2fa25afb181a4178
Poojani <poojaniranasinghe2000@gmail.com>
2023-04-07 06:52:30 +0530

    migration report

--- InHomePlan/InHomePlanWeb/Data/ApplicationDbContext.cs
+++ InHomePlan/InHomePlanWeb/Data/ApplicationDbContext.cs
@@ -17,7 +17,8 @@ namespace InHomePlanWeb.Data
         public DbSet<Status> Status { get; set; }   
         public DbSet<Session> Session { get; set; }
         public DbSet<RegionalStaff> RegionalStaff { get; set; } 
-        public DbSet<Review> Review { get; set; }   
+        public DbSet<Review> Review { get; set; }  
+        public DbSet<Report> Report { get; set; }   
 
 
 
---
+++ InHomePlan/InHomePlanWeb/Migrations/20230407012159_AddTableReportToDb.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InHomePlanWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTableReportToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Report",
+                columns: table => new
+                {
+                    ReportID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ReportName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ApplicationID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Report", x => x.ReportID);
+                    table.ForeignKey(
+                        name: "FK_Report_Application_ApplicationID",
+                        column: x => x.ApplicationID,
+                        principalTable: "Application",
+                        principalColumn: "ApplicationID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Report_ApplicationID",
+                table: "Report",
+                column: "ApplicationID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Report");
+        }
+    }
+}
