65497500b441b2a4d0d4ec7c620986507670b585
Poojani <poojaniranasinghe2000@gmail.com>
2023-04-07 06:01:40 +0530

    configure foreign key

--- InHomePlan/InHomePlanWeb/Data/ApplicationDbContext.cs
+++ InHomePlan/InHomePlanWeb/Data/ApplicationDbContext.cs
@@ -17,6 +17,24 @@ namespace InHomePlanWeb.Data
         public DbSet<Status> Status { get; set; }   
         public DbSet<Session> Session { get; set; }
         public DbSet<RegionalStaff> RegionalStaff { get; set; } 
+        public DbSet<Review> Review { get; set; }
+        // Fluent API
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure foreign key for ApplicationID in Review
+            modelBuilder.Entity<Review>()
+                .HasOne(a => a.Application)
+                .WithMany(r => r.Review)
+                .HasForeignKey(a => a.ApplicationID)
+                .OnDelete(DeleteBehavior.Cascade); // Or whatever behavior you prefer
+
+            // Configure foreign key for RegionalStaffID in Review
+            modelBuilder.Entity<Review>()
+                .HasOne(r => r.RegionalStaff)
+                .WithMany(r => r.Review)
+                .HasForeignKey(r => r.RegionalStaffID)
+                .OnDelete(DeleteBehavior.Cascade); // Or whatever behavior you prefer
+        }
 
 
 
