f81b8468541906af6c84f8375cf3ca4d822e915f
Poojani <poojaniranasinghe2000@gmail.com>
2023-04-07 12:26:53 +0530

    rejection detail model

---
+++ InHomePlan/InHomePlanWeb/Models/Rejection_Detail.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InHomePlanWeb.Models
+{
+    public class Rejection_Detail
+    {
+        [Key]
+        public int RejectionID { get; set; }
+        public string Reason { get; set;}
+        public string RejectedBy { get; set;}
+
+        [ForeignKey("Application")]
+        public int ApplicationID { get; set; }
+
+        public virtual Application Application { get; set; }
+    }
+}
