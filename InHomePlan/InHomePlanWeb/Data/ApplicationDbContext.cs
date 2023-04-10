a80caaf0426287b3efbc9a125c6cca0efc22365d
Poojani <poojaniranasinghe2000@gmail.com>
2023-04-10 20:21:38 +0530

    install entity framework identity

--- InHomePlan/InHomePlanWeb/Data/ApplicationDbContext.cs
+++ InHomePlan/InHomePlanWeb/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using InHomePlanWeb.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace InHomePlanWeb.Data
 {
-    public class ApplicationDbContext : DbContext 
+    public class ApplicationDbContext : IdentityDbContext 
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
