29df206b9b785f265f37b130910763703a736050
Poojani <poojaniranasinghe2000@gmail.com>
2023-04-11 14:45:12 +0530

    configure in program.cs

--- InHomePlan/InHomePlanWeb/Program.cs
+++ InHomePlan/InHomePlanWeb/Program.cs
@@ -11,6 +11,7 @@ options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectio
 
 builder.Services.AddDefaultIdentity<IdentityUser>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -29,6 +30,7 @@ app.UseRouting();
 app.UseAuthentication();;
 
 app.UseAuthorization();
+app.MapRazorPages();
 
 app.MapControllerRoute(
     name: "default",
