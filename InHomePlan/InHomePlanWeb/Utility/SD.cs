e64dc1f07f432210d2d6f7b9947c73e0541543a2
Poojani <poojaniranasinghe2000@gmail.com>
2023-04-12 08:07:21 +0530

    adding user roles

--- InHomePlan/InHomePlanWeb/Utility/SD.cs
+++ InHomePlan/InHomePlanWeb/Utility/SD.cs
@@ -2,6 +2,11 @@
 {
     public class SD
     {
+        public const string Role_User_Housown = "House Owner";
+        public const string Role_User_Staff = "Staff";
+        public const string Role_Admin = "Admin";
+        public const string Role_Sur = "Surveyor";
+        public const string Role_Arch = "Architect";
 
     }
 }
