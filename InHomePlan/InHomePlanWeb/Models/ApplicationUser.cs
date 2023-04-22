45f8f3be056399c9d00c50193e364640e73aa903
Poojani <poojaniranasinghe2000@gmail.com>
2023-04-22 12:28:23 +0530

    application user model

--- InHomePlan/InHomePlanWeb/Models/ApplicationUser.cs
+++ InHomePlan/InHomePlanWeb/Models/ApplicationUser.cs
@@ -6,18 +6,20 @@ namespace InHomePlanWeb.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-        public string Password { get; set; }
-        public string? Email { get; set; }
-       
-        public string? Phone { get; set; }
-        public string? Address { get; set; }
-        public string StreetAddress { get; internal set; }
-        public string City { get; internal set; }
-        public string Name { get; internal set; }
-        public string State { get; internal set; }
-        public string PostalCode { get; internal set; }
+        [System.ComponentModel.DataAnnotations.Required]
+        public string Name { get; set; }
+        public string?StreetAddress { get; internal set; }
+        public string? City { get; internal set; }
+
+        public string? Province { get; internal set; }
+        public string ?PostalCode { get; internal set; }
+        //public string Firstname { get; set; }
+        //public string Lastname { get; set; }
+        //public string Password { get; set; }
+        //public string? Email { get; set; }
+
+        //public string? Phone { get; set; }
+        //public string? Address { get; set; }
+
     }
 }
