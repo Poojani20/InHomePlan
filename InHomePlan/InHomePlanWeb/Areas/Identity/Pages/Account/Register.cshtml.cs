b3486faee89472b3935e78cd41e98892aabe041f
Poojani <poojaniranasinghe2000@gmail.com>
2023-04-22 08:30:23 +0530

    adding more fields

--- InHomePlan/InHomePlanWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ InHomePlan/InHomePlanWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -109,6 +109,14 @@ namespace InHomePlanWeb.Areas.Identity.Pages.Account
 
             [ValidateNever]
             public IEnumerable<SelectListItem> RoleList { get; set; }
+
+            //adding more properties
+
+            public string? StreetAddress { get; set; }
+            public string? City { get; set; }
+            public string? State { get; set; }
+            public string? PostalCode { get; set; }
+            public string? PhoneNumber { get; set; }
         }
 
         //creating user roles using helper method
