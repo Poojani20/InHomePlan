f04ecede341a276a9222261e44558a31424c3185
Poojani <poojaniranasinghe2000@gmail.com>
2023-04-25 10:56:39 +0530

    application backend

--- InHomePlan/InHomePlanWeb/Controllers/ApplicationController.cs
+++ InHomePlan/InHomePlanWeb/Controllers/ApplicationController.cs
@@ -1,16 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using InHomePlanWeb.Models;
+using System.Threading.Tasks;
+using InHomePlanWeb.Data;
 
 namespace InHomePlanWeb.Controllers
 {
     public class ApplicationController : Controller
     {
+
+        private readonly ApplicationDbContext _context;
+
+        public ApplicationController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Application1()
         {
             return View();
         }
-        public IActionResult Application2() 
-        { 
-            return View();
+
+        [HttpPost]
+        public async Task<IActionResult> Application1(Application application)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(application);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(application);
         }
+
     }
 }
