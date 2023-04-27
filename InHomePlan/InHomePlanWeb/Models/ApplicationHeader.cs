c40b28c3ecf2e01edbc1c6606754c4c8ae7fd6c8
Poojani <poojaniranasinghe2000@gmail.com>
2023-04-27 11:00:43 +0530

    application header model adding properties

--- InHomePlan/InHomePlanWeb/Models/ApplicationHeader.cs
+++ InHomePlan/InHomePlanWeb/Models/ApplicationHeader.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.Build.Framework;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace InHomePlanWeb.Models
@@ -18,6 +19,34 @@ namespace InHomePlanWeb.Models
         public string? PaymentStatus { get; set; }
         public string? TrackingNumber { get; set; }
         public string? PaymentIntendId { get; set; }
+        [Required]
+        public String FirstName { get; set; }
+        [Required]
+        public String LastName { get; set; }
+        [Required]
+        public String Address { get; set; }
+        [Required]
+        public String NIC { get; set; }
+        [Required]
+        public String Phone { get; set; }
+        [Required]
+        public String Email { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        public String AssessmentNo { get; set; }
+        [Required]
+        public String PostalCode { get; set; }
+        [Required]
+        public String StreetName { get; set; }
+        [Required]
+        public int No_Of_Rooms { get; set; }
+        [Required]
+        public int No_of_perches { get; set; }
+        [Required]
+        public String BuildingArea { get; set; }
+        [Required]
+        public String PlanNo { get; set; }
 
 
 
