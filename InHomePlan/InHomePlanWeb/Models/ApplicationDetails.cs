dee795bb0fd55c335698591489dd345ff84f8906
Poojani <poojaniranasinghe2000@gmail.com>
2023-04-27 11:01:02 +0530

    application details model

---
+++ InHomePlan/InHomePlanWeb/Models/ApplicationDetails.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InHomePlanWeb.Models
+{
+    public class ApplicationDetails
+    {
+       
+        public int Id { get; set; }
+        [Required]
+        public int ApplicationHeaderId { get; set; }
+        [ForeignKey("ApplicationHeaderId")]
+        [ValidateNever]
+        public ApplicationHeader ApplicationHeader { get; set; }
+
+        [Required]
+        public int ApplicationId { get; set; }
+        [ForeignKey("ApplicationId")]
+        [ValidateNever]
+        public Application Application { get; set; }
+
+
+    }
+}
