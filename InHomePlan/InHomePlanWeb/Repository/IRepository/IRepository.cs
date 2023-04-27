f9a689729f1c055cfdebcedefe2efcfbfa3afdf2
Poojani <poojaniranasinghe2000@gmail.com>
2023-04-27 12:19:06 +0530

    adding repository pattern

---
+++ InHomePlan/InHomePlanWeb/Repository/IRepository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace InHomePlanWeb.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        //T - Application
+        IEnumerable<T> GetAll();
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entity);
+
+    }
+}
---
+++ InHomePlan/InHomePlanWeb/Repository/Repository.cs
@@ -0,0 +1,45 @@
+﻿using InHomePlanWeb.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace InHomePlanWeb.Repository.IRepository;
+
+public class Repository<T> : IRepository<T> where T : class
+
+{
+    public readonly ApplicationDbContext _db;
+    internal DbSet<T> dbSet;
+    public Repository(ApplicationDbContext db)
+    {
+        _db = db;
+        this.dbSet = _db.Set<T>();
+       // _db.Application == dbSet
+    }
+
+    public void Add(T entity)
+    {
+        dbSet.Add(entity);
+    }
+
+    public T Get(Expression<Func<T, bool>> filter)
+    {
+        IQueryable<T> query = dbSet;
+        query = query.Where(filter);
+        return query.FirstOrDefault();
+    }
+
+    public IEnumerable<T> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Remove(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveRange(IEnumerable<T> entity)
+    {
+        throw new NotImplementedException();
+    }
+}
