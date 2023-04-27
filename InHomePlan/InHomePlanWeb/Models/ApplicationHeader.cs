75c607b83065b699e3d368863ad64dfa17577d26
Poojani <poojaniranasinghe2000@gmail.com>
2023-04-27 10:50:51 +0530

    application header

---
+++ InHomePlan/InHomePlanWeb/Models/ApplicationHeader.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InHomePlanWeb.Models
+{
+    public class ApplicationHeader
+    {
+        public int Id { get; set; }
+        public string ApplicationUserId { get; set; }
+        [ForeignKey("ApplicationUserId")]
+        [ValidateNever]
+        public ApplicationUser ApplicationUser { get; set; }
+        public DateTime SumbitDate { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public double Total { get; set; }
+
+        public string? ApplicationStatus { get; set; }
+        public string? PaymentStatus { get; set; }
+        public string? TrackingNumber { get; set; }
+        public string? PaymentIntendId { get; set; }
+
+
+
+        // public string? Carrier { get; set; }
+    }
+}
