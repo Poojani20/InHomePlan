62c69c4fe59352988754f853c49bbf1481201318
Poojani <poojaniranasinghe2000@gmail.com>
2023-04-29 14:36:31 +0530

    application detail and header repos

---
+++ InHomePlan/InHomePlanWeb/Repository/ApplicationDetailRepository.cs
@@ -0,0 +1,26 @@
+﻿using InHomePlanWeb.Data;
+using InHomePlanWeb.Models;
+using InHomePlanWeb.Repository.IRepository;
+using System.Linq.Expressions;
+
+namespace InHomePlanWeb.Repository
+{
+    public class ApplicationDetailRepository : Repository<ApplicationDetails>, IApplicationDetailRepository 
+    {
+        private ApplicationDbContext _db;
+        public ApplicationDetailRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(ApplicationDetails obj)
+        {
+            _db.ApplicationDetails.Update(obj);
+        }
+    }
+}
---
+++ InHomePlan/InHomePlanWeb/Repository/ApplicationHeaderRepository.cs
@@ -0,0 +1,26 @@
+﻿using InHomePlanWeb.Data;
+using InHomePlanWeb.Models;
+using InHomePlanWeb.Repository.IRepository;
+using System.Linq.Expressions;
+
+namespace InHomePlanWeb.Repository
+{
+    public class ApplicationHeaderRepository : Repository<ApplicationHeader>, IApplicationHeaderRepository 
+    {
+        private ApplicationDbContext _db;
+        public ApplicationHeaderRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(ApplicationHeader obj)
+        {
+            _db.ApplicationHeader.Update(obj);
+        }
+    }
+}
