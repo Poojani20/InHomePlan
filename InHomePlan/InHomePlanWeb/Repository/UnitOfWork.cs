f740b22ddbe45131c6e9db82ea0f4dbb1d591916
Poojani <poojaniranasinghe2000@gmail.com>
2023-04-29 20:50:02 +0530

    unit of work class

---
+++ InHomePlan/InHomePlanWeb/Repository/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using InHomePlanWeb.Data;
+using InHomePlanWeb.Models;
+using InHomePlanWeb.Repository.IRepository;
+
+namespace InHomePlanWeb.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private ApplicationDbContext _db;
+
+        
+        public IApplicationRepository Application { get; private set; }
+        public UnitOfWork(ApplicationDbContext db) 
+        {
+            _db = db;
+            Application = new ApplicationRepository(_db);
+        }
+       
+        public void save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
