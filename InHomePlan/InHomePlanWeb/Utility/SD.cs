a28cbc09446d748c39ce29cabd792ebf7fdd6a3e
Poojani <poojaniranasinghe2000@gmail.com>
2023-05-03 12:02:03 +0530

    status

--- InHomePlan/InHomePlanWeb/Utility/SD.cs
+++ InHomePlan/InHomePlanWeb/Utility/SD.cs
@@ -5,8 +5,17 @@
         public const string Role_Houseowner = "House Owner";
         public const string Role_Staff = "Staff";
         public const string Role_Admin = "Admin";
-        public const string Role_Surveyor = "Surveyor";
-        public const string Role_Architect = "Architect";
+        //public const string Role_Surveyor = "Surveyor";
+        //public const string Role_Architect = "Architect";
 
+        public const string StatusPending = "Pending";
+        public const string StatusApproved = "Approved";
+        public const string StatusInProcess = "Processing";
+        public const string StatusRejected = "Rejected";
+
+        public const string PaymentStatusPending = "Pending";
+        public const string PaymentStatusApproved = "Approved";
+        public const string PayementStatusDelayedPayment = "ApprovedForDelayedPayment";
+        public const string PaymentStatusRejected = "Rejected";
     }
 }
