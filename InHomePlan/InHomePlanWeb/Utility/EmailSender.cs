b378360005a830a50a07160827e789b8c0174cad
Poojani20 <poojaniranasinghe2000@gmail.com>
2023-05-09 21:03:04 +0930

    configured sendgrid emails

--- InHomePlan/InHomePlanWeb/Utility/EmailSender.cs
+++ InHomePlan/InHomePlanWeb/Utility/EmailSender.cs
@@ -1,13 +1,27 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace InHomePlanWeb.Utility
 {
     public class EmailSender : IEmailSender
     {
+        public string SendGridSecret { get; set; }
+
+        public EmailSender(IConfiguration _config)
+        {
+            SendGridSecret = _config.GetValue<string>("SendGrid:SecretKey");
+        }
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
             //logic to send email
-            return Task.CompletedTask;
+            var client = new SendGridClient(SendGridSecret);
+            var from = new EmailAddress("hasitha.ranasinghearachchige@flinders.edu.au", "InHomePlan");
+            var to = new EmailAddress(email);
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+            
+            return client.SendEmailAsync(msg);
+            //return Task.CompletedTask;
         }
     }
 }
